35723117c358a8b4ee93ebcf332b7f96f90508f8
Dylan <52908667+smellycloud@users.noreply.github.com>
2024-12-11 11:17:57 +0100

    update ensembles

--- ensembles/cruel_summer/main.py
+++ ensembles/cruel_summer/main.py
@@ -3,13 +3,12 @@ import warnings
 from pathlib import Path
 from views_pipeline_core.cli.utils import parse_args, validate_arguments
 from views_pipeline_core.logging.utils import setup_logging
-from views_pipeline_core.managers.path_manager import EnsemblePath
-from views_pipeline_core.managers.ensemble_manager import EnsembleManager
+from views_pipeline_core.managers.ensemble import EnsemblePathManager, EnsembleManager
 
 warnings.filterwarnings("ignore")
 
 try:
-    ensemble_path = EnsemblePath(Path(__file__))
+    ensemble_path = EnsemblePathManager(Path(__file__))
 except Exception as e:
     raise RuntimeError(f"An unexpected error occurred: {e}.")
 
@@ -20,5 +19,5 @@ if __name__ == "__main__":
     wandb.login()
     args = parse_args()
     validate_arguments(args)
-    
+
     EnsembleManager(ensemble_path=ensemble_path).execute_single_run(args)
