32c57676b97746db499ceb0f695e7600a84d65f3
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-01-22 23:45:12 +0100

    fix typo

--- models/ominous_ox/main.py
+++ models/ominous_ox/main.py
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     if args.sweep:
         StepshifterManager(model_path=model_path).execute_sweep_run(args)
     else:
-        StepshifterManager(model_path=model_path, wandb_notification=True).execute_single_run(args)
+        StepshifterManager(model_path=model_path).execute_single_run(args)
