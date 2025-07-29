2272477ed7d64144925c752fcc422a2a66560e42
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-07-29 16:13:18 +0200

    add red_panda (tide-cm)

---
+++ models/red_panda/README.md
@@ -0,0 +1,3 @@
+# Model README
+## Model name: red_panda
+## Created on: 2025-07-29 16:03:31.634281
---
+++ models/red_panda/main.py
@@ -0,0 +1,32 @@
+import wandb
+import warnings
+from pathlib import Path
+from views_pipeline_core.cli.utils import parse_args, validate_arguments
+from views_pipeline_core.managers.model import ModelPathManager
+
+# Import your model manager class here
+# E.g. from views_stepshifter.manager.stepshifter_manager import StepshifterManager
+
+warnings.filterwarnings("ignore")
+
+try:
+    model_path = ModelPathManager(Path(__file__))
+except FileNotFoundError as fnf_error:
+    raise RuntimeError(
+        f"File not found: {fnf_error}. Check the file path and try again."
+    )
+except PermissionError as perm_error:
+    raise RuntimeError(
+        f"Permission denied: {perm_error}. Check your permissions and try again."
+    )
+except Exception as e:
+    raise RuntimeError(f"Unexpected error: {e}. Check the logs for details.")
+
+if __name__ == "__main__":
+    wandb.login()
+    args = parse_args()
+    validate_arguments(args)
+    if args.sweep:
+        # YourModelManager(model_path=model_path).execute_sweep_run(args)
+    else:
+        # YourModelManager(model_path=model_path).execute_single_run(args)
