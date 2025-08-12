62678766dd2eb9a91af6052953157b7a20d40392
Sonhaf-prio <sonhaf@prio.org>
2025-08-12 14:01:14 +0200

    renamed folders to indicate loa

--- models/average_cmbaseline/configs/config_hyperparameters.py
+++ models/average_cmbaseline/configs/config_hyperparameters.py
@@ -10,7 +10,7 @@ def get_hp_config():
     
     hyperparameters = {
         'steps': [*range(1, 36 + 1, 1)],
-        'months':6,
+        'months':60,
         # Add more hyperparameters as needed
     }
     return hyperparameters
