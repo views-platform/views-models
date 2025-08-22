222ee78b79c30b963eec75dc2c80fd7a5860c030
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-08-23 00:32:33 +0200

    update metrics

--- ensembles/white_mustang/configs/config_meta.py
+++ ensembles/white_mustang/configs/config_meta.py
@@ -9,7 +9,7 @@ def get_meta_config():
     meta_config = {
         "name": "white_mustang",
         "models": ["lavender_haze", "blank_space"],
-        "metrics": ["RMSLE", "CRPS"],
+        "metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar"],
         "targets": "ln_ged_sb_dep",  # Double-check the target variables of each model
         "level": "pgm",
         "aggregation": "mean",
