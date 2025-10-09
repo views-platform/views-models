793af53c779dc7da108dc8c39dbe227959481b33
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-10-09 14:39:49 +0200

    fao api boilerplate

---
+++ apis/un_fao/README.md
@@ -0,0 +1,3 @@
+# Model README
+## Model name: un_fao
+## Created on: 2025-10-09 12:45:58.050855
---
+++ apis/un_fao/configs/config_meta.py
@@ -0,0 +1,15 @@
+def get_meta_config():
+    """
+    Contains the meta data for the model (model algorithm, name, target variable, and level of analysis).
+    This config is for documentation purposes only, and modifying it will not affect the model, the training, or the evaluation.
+
+    Returns:
+    - meta_config (dict): A dictionary containing model meta configuration.
+    """
+    
+    meta_config = {
+        "name": "un_fao", 
+        "algorithm": "API",
+        # "creator": "Your name here",
+    }
+    return meta_config
