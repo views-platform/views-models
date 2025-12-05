189f29aef868a975308824457335cc69033da93a
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-12-05 13:28:32 +0100

    cleanup qs

--- models/dancing_queen/configs/config_queryset.py
+++ models/dancing_queen/configs/config_queryset.py
@@ -26,13 +26,13 @@ def generate():
                     from_column="ged_sb_best_sum_nokgi",
                 ).transform.missing.fill()
             )
-            .with_column(
-                Column(
-                    "lr_ged_sb",
-                    from_loa="country_month",
-                    from_column="ged_sb_best_sum_nokgi",
-                ).transform.missing.fill()
-            )
+            # .with_column(
+            #     Column(
+            #         "lr_ged_sb",
+            #         from_loa="country_month",
+            #         from_column="ged_sb_best_sum_nokgi",
+            #     ).transform.missing.fill()
+            # )
             .with_column(
                 Column(
                     "lr_ged_ns",
