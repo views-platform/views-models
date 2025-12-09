0fd487d9dc93a98713aa14afda948c5f720c93c2
Sonhaf-prio <sonhaf@prio.org>
2025-12-09 09:37:10 +0100

    simplified hp

--- models/heat_waves/configs/config_hyperparameters.py
+++ models/heat_waves/configs/config_hyperparameters.py
@@ -6,11 +6,9 @@ def get_hp_config():
     Returns:
     - hyperparameters (dict): A dictionary containing hyperparameters for training the model, which determine the model's behavior during the training phase.
     """
-    
+
     hyperparameters = {
-        'steps': [*range(1, 36 + 1, 1)],
-
-
+        "steps": [*range(1, 36 + 1, 1)],
         # royal-sweep-63
         # "batch_size": 256,
         # "attention_dropout": 0.4,
@@ -33,7 +31,6 @@ def get_hp_config():
         # "use_reversible_instance_norm": True,
         # "weight_decay": 0.00001339740219226816,
         # "zero_threshold": 0.013101193138707702,
-
         # rural-sweep-287
         "attention_dropout": 0.4,
         "batch_size": 128,
@@ -44,20 +41,19 @@ def get_hp_config():
         "feature_scaler": "MaxAbsScaler",
         "feed_forward": "GELU",
         "full_attention": False,
-        "hidden_size": 512,
+        "hidden_size": 128,
         "input_chunk_length": 60,
         "loss_function": "WeightedPenaltyHuberLoss",
-        "lr": 0.000021841168537191,
-        "lstm_layers": 3,
+        "lr": 0.0001,
+        "lstm_layers": 1,
         "n_epochs": 300,
         "non_zero_weight": 11.810582310673835,
-        "num_attention_heads": 2,
-        "target_scaler": "MinMaxScaler",
+        "num_attention_heads": 4,
+        "target_scaler": "RobustScaler",
         "use_reversible_instance_norm": False,
         "weight_decay": 0.00002389357463410524,
         "zero_threshold": 0.0881212976698389,
-
         "num_samples": 1,
-        "mc_dropout": True,
+        "mc_dropout": False,
     }
     return hyperparameters
