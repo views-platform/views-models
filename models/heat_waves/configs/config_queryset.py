d78b42c4e71316ff1520b80535a89264918dc599
Sonhaf-prio <sonhaf@prio.org>
2025-12-12 13:59:39 +0100

    new params for sweep

--- models/heat_waves/configs/config_queryset.py
+++ models/heat_waves/configs/config_queryset.py
@@ -16,6 +16,23 @@ def generate():
 
     # VIEWSER 6, Example configuration. Modify as needed.
 
+    def _add_minimal_features(queryset: Queryset) -> Queryset:
+        return queryset.with_column(
+            Column(
+                "lr_ged_sb_dep",
+                from_loa="country_month",
+                from_column="ged_sb_best_sum_nokgi",
+            ).transform.missing.fill()
+        ).with_column(
+            Column(
+                "lr_ged_sb",
+                from_loa="country_month",
+                from_column="ged_sb_best_sum_nokgi",
+            )
+            .transform.missing.fill()
+            .transform.ops.ln()
+        )
+
     def _add_conflict_history(queryset: Queryset) -> Queryset:
         print("Adding conflict history features...")
         return (
@@ -729,5 +746,6 @@ def generate():
 
     queryset = Queryset(f"{model_name}", "country_month")
 
-    return _add_topics(_add_vdem(_add_wdi(_add_conflict_history(queryset))))
+    # return _add_topics(_add_vdem(_add_wdi(_add_conflict_history(queryset))))
+    return _add_minimal_features(queryset)
     # return _add_topics(_add_conflict_history(queryset=queryset))
