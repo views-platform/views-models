13080ac4cb6f90f39872a3de3418b5a708145ef6
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2025-12-15 14:19:49 +0100

    hyperparameters

--- models/elastic_heart/main.py
+++ models/elastic_heart/main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from views_pipeline_core.cli import ForecastingModelArgs
 from views_pipeline_core.managers import ModelPathManager
 from views_r2darts2.manager.model import DartsForecastingModelManager
-# from views_r2darts2.manager.model import DartsLikelihoodForecastingModelManager
+from views_r2darts2.manager.model import DartsLikelihoodForecastingModelManager
 
 warnings.filterwarnings("ignore")
 
@@ -18,8 +18,8 @@ except Exception as e:
 if __name__ == "__main__":
     args = ForecastingModelArgs.parse_args()
     if args.sweep:
-        DartsForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_sweep_run(args)
-        # DartsLikelihoodForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_sweep_run(args)
+        # DartsForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_sweep_run(args)
+        DartsLikelihoodForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_sweep_run(args)
     else:
-        DartsForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_single_run(args)
-        # DartsLikelihoodForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_single_run(args)
+        # DartsForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_single_run(args)
+        DartsLikelihoodForecastingModelManager(model_path=model_path, wandb_notifications=False).execute_single_run(args)
