e97c93086b197f29080529d99609d6579174b4d8
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-02-05 13:13:45 +0100

    oops

--- models/dancing_queen/configs/config_meta.py
+++ models/dancing_queen/configs/config_meta.py
@@ -11,7 +11,7 @@ def get_meta_config():
         "name": "dancing_queen", 
         "algorithm": "BlockRNNModel",
         # Uncomment and modify the following lines as needed for additional metadata:
-        "targets": ["ged_sb_dep"],
+        "targets": ["lr_ged_sb"],
         # "queryset": "escwa001_cflong",
         "level": "cm",
         "creator": "Dylan",
