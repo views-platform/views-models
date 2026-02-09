c9677b44059290e66cc2e9c368177632f51e12fe
Polichinl <simmaa@prio.org>
2026-02-09 12:05:16 +0100

    trying to plug this in to pipeline core and eval..

--- models/purple_alien/configs/config_meta.py
+++ models/purple_alien/configs/config_meta.py
@@ -9,9 +9,13 @@ def get_meta_config():
     meta_config = {
         "name": "purple_alien",
         "algorithm": "HydraNet", 
-        "targets": ["ln_sb_best", "ln_ns_best", "ln_os_best", "ln_sb_best_binarized", "ln_ns_best_binarized", "ln_os_best_binarized"], 
-        "queryset": "escwa001_cflong",
+        "targets": ["lr_sb_best", "lr_ns_best", "lr_os_best"], #, "ln_sb_best_binarized", "ln_ns_best_binarized", "ln_os_best_binarized"], 
+        # "queryset": "escwa001_cflong",
+        "identity_cols" : ["priogrid_gid", "col", "row", "month_id", "c_id"],
+        "index_names": ['month_id', 'priogrid_gid'],
+        "features" : ["lr_sb_best", "lr_ns_best", "lr_os_best"],
         "level": "pgm",
-        "creator": "Simon" 
+        "creator": "Simon",
+        "metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar", "AP"],
     }
     return meta_config 
