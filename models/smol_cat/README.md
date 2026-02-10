31993e409d512537483f4412660775a596a7e472
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-02-10 13:09:00 +0100

    tweedie

---
+++ models/smol_cat/README.md
@@ -0,0 +1,3 @@
+# Model README
+## Model name: smol_cat
+## Created on: 2026-02-10 19:40:54.074965
---
+++ models/smol_cat/configs/config_meta.py
@@ -0,0 +1,20 @@
+def get_meta_config():
+    """
+    Contains the meta data for the model (model algorithm, name, target variable, and level of analysis).
+    This config is for documentation purposes only, and modifying it will not affect the model, the training, or the evaluation.
+
+    Returns:
+    - meta_config (dict): A dictionary containing model meta configuration.
+    """
+    
+    meta_config = {
+        "name": "smol_cat", 
+        "algorithm": "TiDEModel",
+        # Uncomment and modify the following lines as needed for additional metadata:
+        "targets": ["lr_ged_sb"],
+        # "queryset": "escwa001_cflong",
+        "level": "cm",
+        "creator": "Dylan",
+        "metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar", "MTD"],
+    }
+    return meta_config
