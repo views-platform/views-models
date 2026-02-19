e69c552d4cd7cd9ffb01e74fa45a908322e050d8
Sonhaf-prio <sonhaf@prio.org>
2026-02-19 16:02:03 +0100

    settings for testing the api locally and on fimbulthul

---
+++ apis/un_fao/configs/config_deployment.py
@@ -0,0 +1,28 @@
+"""
+Deployment Configuration Script
+
+This script defines the deployment configuration settings for the application.
+It includes the deployment status and any additional settings specified.
+
+Deployment Status:
+- shadow: The deployment is shadowed and not yet active.
+- deployed: The deployment is active and in use.
+- baseline: The deployment is in a baseline state, for reference or comparison.
+- deprecated: The deployment is deprecated and no longer supported.
+
+Additional settings can be included in the configuration dictionary as needed.
+
+"""
+
+
+def get_deployment_config():
+    # Deployment settings
+    deployment_config = {
+        "deployment_status": "shadow",
+        "port": 8000,
+        "host": "0.0.0.0",
+        "action": "start",
+        "workers": 1,
+        "reload": "false",
+    }
+    return deployment_config
---
+++ apis/un_fao/main.py
@@ -0,0 +1,34 @@
+import wandb
+import warnings
+from pathlib import Path
+from views_faoapi.managers.model import APIPathManager
+
+# from views_faoapi.managers.api import FAOApiManager
+from views_faoapi.managers.api import FAOApiManager
+
+
+warnings.filterwarnings("ignore")
+
+try:
+    model_path = APIPathManager(Path(__file__))
+except FileNotFoundError as fnf_error:
+    raise RuntimeError(
+        f"File not found: {fnf_error}. Check the file path and try again."
+    )
+except PermissionError as perm_error:
+    raise RuntimeError(
+        f"Permission denied: {perm_error}. Check your permissions and try again."
+    )
+except Exception as e:
+    raise RuntimeError(f"Unexpected error: {e}. Check the logs for details.")
+
+if __name__ == "__main__":
+    wandb.login()
+    # args = parse_args()
+
+    manager = FAOApiManager(
+        model_path=model_path,
+        wandb_notifications=False,
+    )
+
+    manager.run()
