8583a54c491e18096f07afd9530a19144f80c9ef
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-02-19 13:29:17 +0100

    old

--- models/revolving_door/configs/config_meta.py
+++ models/revolving_door/configs/config_meta.py
@@ -11,9 +11,11 @@ def get_meta_config():
         "name": "revolving_door", 
         "algorithm": "NHiTSModel",
         # Uncomment and modify the following lines as needed for additional metadata:
-        "regression_targets": ["lr_ged_sb", "lr_ged_ns", "lr_ged_os"],
+        # "regression_targets": ["lr_ged_sb", "lr_ged_ns", "lr_ged_os"],
+        "targets": ["lr_ged_sb", "lr_ged_ns", "lr_ged_os"],
         "level": "cm",
         "creator": "Dylan",
-        "regression_metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar", "MTD", "BCD", "Pearson", "LevelRatio"],
+        # "regression_metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar", "MTD", "BCD", "Pearson", "LevelRatio"],
+        "metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar", "MTD", "BCD", "Pearson", "LevelRatio"],
     }
     return meta_config
