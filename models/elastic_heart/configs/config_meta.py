0f753286fa7bc11ba5785ada8b84ae901fe4546f
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-04-16 00:47:09 +0200

    remove crps

--- models/elastic_heart/configs/config_meta.py
+++ models/elastic_heart/configs/config_meta.py
@@ -14,6 +14,6 @@ def get_meta_config():
         "regression_targets": ["lr_ged_sb", "lr_ged_ns", "lr_ged_os"],
         "level": "cm",
         "creator": "Dylan",
-        "regression_point_metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar"],
+        "regression_point_metrics": ["RMSLE", "MSE", "MSLE", "y_hat_bar"],
     }
     return meta_config
--- models/good_life/configs/config_meta.py
+++ models/good_life/configs/config_meta.py
@@ -15,6 +15,6 @@ def get_meta_config():
         # "queryset": "escwa001_cflong",
         "level": "cm",
         "creator": "Dylan",
-        "regression_point_metrics": ["RMSLE", "CRPS", "MSE", "MSLE", "y_hat_bar"],
+        "regression_point_metrics": ["RMSLE", "MSE", "MSLE", "y_hat_bar"],
     }
     return meta_config
