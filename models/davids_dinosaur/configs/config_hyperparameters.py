d40b98d70c087d2c2d1f8b23d1b92c47d665d5b3
Luuk Boekestein <luuk.boekestein@gmail.com>
2026-04-16 16:15:46 +0200

    add views models for all 4 markov variations

---
+++ models/davids_dinosaur/configs/config_hyperparameters.py
@@ -0,0 +1,31 @@
+
+def get_hp_config():
+    """
+    Contains the hyperparameter configurations for model training.
+    This configuration is "operational" so modifying these settings will impact the model's behavior during the training.
+
+    Returns:
+    - hyperparameters (dict): A dictionary containing hyperparameters for training the model, which determine the model's behavior during the training phase.
+    """
+    
+    hyperparameters = {
+        'steps': [*range(1, 36 + 1, 1)],
+        'markov_target': 'ln_ged_sb_dep',
+        'markov_threshold': 0,
+        'markov_method': 'direct',
+        'regression_method': 'multi',
+        'random_state': 42,
+        'n_jobs': -1,
+        'verbose': True,
+        'rf_class_params': {
+            'n_estimators': 500,
+        },
+        'rf_reg_params': {
+            'n_estimators': 500,
+            'max_features': 'sqrt',
+            'min_samples_split': 2,
+            'min_samples_leaf': 5,
+        },
+    }
+
+    return hyperparameters
---
+++ models/sleeping_beauty/README.md
@@ -0,0 +1,3 @@
+# Model README
+## Model name: sleeping_beauty
+## Created on: 2026-03-26 21:18:08.404163
