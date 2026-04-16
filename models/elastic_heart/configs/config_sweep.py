b39fa320594f0627cef4eb1714e0254eaff3b6f9
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-04-16 23:39:42 +0200

    v9

--- models/elastic_heart/configs/config_sweep.py
+++ models/elastic_heart/configs/config_sweep.py
@@ -4,7 +4,7 @@ def get_sweep_config():
     """
     sweep_config = {
         "method": "bayes",
-        "name": "elastic_heart_tsmixer_spotlight_v8_msle",
+        "name": "elastic_heart_tsmixer_spotlight_v9_msle",
         "early_terminate": {"type": "hyperband", "min_iter": 30, "eta": 2},
         "metric": {"name": "time_series_wise_msle_mean_sb", "goal": "minimize"},
     }
