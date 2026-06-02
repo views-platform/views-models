60941fc3d2a033a627073ffc0ae4fead7115033f
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-06-02 12:20:04 +0200

    tanh

--- models/new_rules/configs/config_hyperparameters.py
+++ models/new_rules/configs/config_hyperparameters.py
@@ -15,7 +15,7 @@ def get_hp_config():
         "layer_widths": 128,
         "expansion_coefficient_dim": 32,
         "trend_polynomial_degree": 2,
-        "activation": "LeakyReLU",
+        "activation": "Tanh",
         "dropout": 0.3,
         "batch_norm": False,
         "use_reversible_instance_norm": True,
