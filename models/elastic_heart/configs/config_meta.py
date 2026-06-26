1bbbaf52f0f37fa9ade2483c3ba4e642a40d1dc9
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-06-26 16:17:13 +0200

    exp: try 1 model, 3 targets

--- models/elastic_heart/configs/config_meta.py
+++ models/elastic_heart/configs/config_meta.py
@@ -11,7 +11,7 @@ def get_meta_config():
         "name": "elastic_heart", 
         "algorithm": "TSMixerModel",
         # Uncomment and modify the following lines as needed for additional metadata:
-        "regression_targets": ["lr_ged_sb"],
+        "regression_targets": ["lr_ged_sb", "lr_ged_ns", "lr_ged_os"],
         "level": "cm",
         "creator": "Dylan",
         "regression_point_baselines": ["average_cmbaseline", "zero_cmbaseline", "locf_cmbaseline"],
