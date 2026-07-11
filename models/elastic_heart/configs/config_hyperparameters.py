9dc6830452261220adb8a63e3fa1bfaab90c4238
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-07-11 20:08:38 +0200

    reset

--- models/elastic_heart/configs/config_hyperparameters.py
+++ models/elastic_heart/configs/config_hyperparameters.py
@@ -55,7 +55,7 @@ def get_hp_config():
             "weight_decay": 5e-5,
         },
         "checkpoint_mode": "best",
-        "loss_function": "SpotlightLoss",
+        "loss_function": "SpotlightLossLogcosh",
         "non_zero_threshold": 0.88,
 
         # Scaling
