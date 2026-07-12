c1fea546b830a773ce3c1e87094246f14759e055
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-07-12 16:46:09 +0200

    val loss

--- models/elastic_heart/configs/config_hyperparameters.py
+++ models/elastic_heart/configs/config_hyperparameters.py
@@ -23,8 +23,8 @@ def get_hp_config():
         # Training
         "batch_size": 128,
         "n_epochs": 300,
-        "early_stopping_monitor": "val_loss",
-        "lr_scheduler_monitor": "val_loss",
+        "early_stopping_monitor": "val_metrics/val_loss",
+        "lr_scheduler_monitor": "val_metrics/val_loss",
         "early_stopping_patience": 12,
         "early_stopping_min_delta": 0.002,
         "force_reset": True,
