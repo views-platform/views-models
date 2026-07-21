04fef7d9b8afcf82df5e66a831e501f8e417c25c
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-07-21 14:37:42 +0200

    trim all qs

--- models/dancing_queen/configs/config_queryset.py
+++ models/dancing_queen/configs/config_queryset.py
@@ -90,24 +90,24 @@ def generate():
             #     .transform.missing.replace_na()
             # )
             # ==================== SPATIAL SPILLOVER ====================
-            .with_column(
-                Column("lr_splag_1_ged_sb", from_loa="country_month", from_column="ged_sb_best_sum_nokgi")
-                .transform.missing.replace_na()
-                .transform.spatial.countrylag(1, 1, 0, 0)
-                .transform.missing.replace_na()
-            )
-            .with_column(
-                Column("lr_splag_1_ged_ns", from_loa="country_month", from_column="ged_ns_best_sum_nokgi")
-                .transform.missing.replace_na()
-                .transform.spatial.countrylag(1, 1, 0, 0)
-                .transform.missing.replace_na()
-            )
-            .with_column(
-                Column("lr_splag_1_ged_os", from_loa="country_month", from_column="ged_os_best_sum_nokgi")
-                .transform.missing.replace_na()
-                .transform.spatial.countrylag(1, 1, 0, 0)
-                .transform.missing.replace_na()
-            )
+            # .with_column(
+            #     Column("lr_splag_1_ged_sb", from_loa="country_month", from_column="ged_sb_best_sum_nokgi")
+            #     .transform.missing.replace_na()
+            #     .transform.spatial.countrylag(1, 1, 0, 0)
+            #     .transform.missing.replace_na()
+            # )
+            # .with_column(
+            #     Column("lr_splag_1_ged_ns", from_loa="country_month", from_column="ged_ns_best_sum_nokgi")
+            #     .transform.missing.replace_na()
+            #     .transform.spatial.countrylag(1, 1, 0, 0)
+            #     .transform.missing.replace_na()
+            # )
+            # .with_column(
+            #     Column("lr_splag_1_ged_os", from_loa="country_month", from_column="ged_os_best_sum_nokgi")
+            #     .transform.missing.replace_na()
+            #     .transform.spatial.countrylag(1, 1, 0, 0)
+            #     .transform.missing.replace_na()
+            # )
         )
 
     def _add_wdi(queryset: Queryset) -> Queryset:
