72cbd8fda65be3b5cb8a6df94a606cb78a5a08e0
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-07-22 23:19:16 +0200

    hmm

--- models/dark_necessities/configs/config_hyperparameters.py
+++ models/dark_necessities/configs/config_hyperparameters.py
@@ -25,7 +25,7 @@ def get_hp_config():
         "num_decoder_layers": 1,
         "use_layer_norm": True,
         "use_reversible_instance_norm": True,
-        "dropout": 0.15,
+        "dropout": 0.05,
         "use_static_covariates": True,
 
         # Training
@@ -41,6 +41,7 @@ def get_hp_config():
         "optimizer_kwargs": {
             "lr": 1e-4,
             "weight_decay": 5e-5,
+            "betas": (0.9, 0.95), 
         },
 
 # LR Scheduler
--- models/mister_bluesky/configs/config_hyperparameters.py
+++ models/mister_bluesky/configs/config_hyperparameters.py
@@ -52,6 +52,7 @@ def get_hp_config():
         "optimizer_kwargs": {
             "lr": 1.5e-4,
             "weight_decay": 5e-5,
+            "betas": (0.9, 0.95), 
         },
         "checkpoint_mode": "best",
         "loss_function": "SpotlightLossLogcosh",
