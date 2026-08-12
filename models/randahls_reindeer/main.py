d4bedc636af23bc0e613fe1d69de12f1cf6aa571
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-12 19:04:47 +0200

    main.py

--- models/randahls_reindeer/main.py
+++ models/randahls_reindeer/main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from views_pipeline_core.cli import ForecastingModelArgs
-from views_pipeline_core.managers.model import ModelPathManager
-from views_markov.manager.markovmodel_manager import MarkovModelManager
+from views_pipeline_core.managers import ModelPathManager
+
+from views_r2darts2 import DartsForecastingModelManager
 
 try:
     model_path = ModelPathManager(Path(__file__))
@@ -10,14 +11,14 @@ except Exception as e:
 
 if __name__ == "__main__":
     args = ForecastingModelArgs.parse_args()
-
-    manager = MarkovModelManager(
-        model_path=model_path,
-        wandb_notifications=args.wandb_notifications,
-        use_prediction_store=args.prediction_store,
-    )
     
     if args.sweep:
-        manager.execute_sweep_run(args)
+        DartsForecastingModelManager(
+            model_path=model_path, 
+            wandb_notifications=args.wandb_notifications
+        ).execute_sweep_run(args)
     else:
-        manager.execute_single_run(args)
+        DartsForecastingModelManager(
+            model_path=model_path, 
+            wandb_notifications=args.wandb_notifications
+        ).execute_single_run(args)
