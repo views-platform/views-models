e5b74d28cd7cf36ebccc308ca18b3a0d29294f7a
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-14 14:24:45 +0200

    gl gelu

--- models/good_life/configs/config_hyperparameters.py
+++ models/good_life/configs/config_hyperparameters.py
@@ -15,7 +15,7 @@ def get_hp_config():
         "detect_anomaly": False,
         "time_steps": 36,  # Checksum: Must match len(steps)
 
-        "activation": "SwiGLU",
+        "activation": "GELU",
         "batch_size": 128,
         "d_model": 96,
         "dim_feedforward": 256,
