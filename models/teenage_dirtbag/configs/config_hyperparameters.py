976aece45f27e48ba9ca2e1a672e2ff3f09a9b4d
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-24 20:27:13 +0200

    tcn

--- models/teenage_dirtbag/configs/config_hyperparameters.py
+++ models/teenage_dirtbag/configs/config_hyperparameters.py
@@ -37,8 +37,8 @@ def get_hp_config():
         # Optimizer
         "optimizer_cls": "AdamW",
         "lr": 1e-4,
-        "weight_decay": 1e-3,
-        "gradient_clip_val": 1.0,
+        "weight_decay": 1e-4,
+        "gradient_clip_val": 10.0,
 
         # LR Scheduler
         "lr_scheduler_cls": "ReduceLROnPlateau",
@@ -57,7 +57,7 @@ def get_hp_config():
         "optimizer_kwargs": {
             "betas": (0.9, 0.999), 
             "lr": 1e-4,
-            "weight_decay": 1e-3,
+            "weight_decay": 1e-4,
         },
 
         # SpotlightLossLogcosh
