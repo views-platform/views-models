4bfd11d44f34e9dbc4f60ef55ce4d1a11f75bab8
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-30 23:28:09 +0200

    decay

--- models/new_rules/configs/config_hyperparameters.py
+++ models/new_rules/configs/config_hyperparameters.py
@@ -37,12 +37,12 @@ def get_hp_config():
         # --- Optimizer ---
         "optimizer_cls": "AdamW",
         "lr": 1e-4,
-        "weight_decay": 1e-4,
-        "gradient_clip_val": 10.0,
+        "weight_decay": 1e-3,
+        "gradient_clip_val": 5.0,
         "optimizer_kwargs": {
             "betas": (0.9, 0.999), 
             "lr": 1e-4,
-            "weight_decay": 1e-4,
+            "weight_decay": 1e-3,
         },
 
         # --- LR Scheduler ---
