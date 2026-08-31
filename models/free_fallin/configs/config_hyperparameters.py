184efe0a3bc2d1a5cc08fcf10443de6b344436bc
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-31 11:19:38 +0200

    reset

--- models/free_fallin/configs/config_hyperparameters.py
+++ models/free_fallin/configs/config_hyperparameters.py
@@ -32,8 +32,8 @@ def get_hp_config():
         # Optimizer
         "optimizer_cls": "AdamW",
         "lr": 3e-4,
-        "weight_decay": 5e-4,
-        "gradient_clip_val": 10.0,
+        "weight_decay": 5e-5,
+        "gradient_clip_val": 20.0,
 
         # LR Scheduler
         "lr_scheduler_cls": "ReduceLROnPlateau",
@@ -53,7 +53,7 @@ def get_hp_config():
         "optimizer_kwargs": {
             "betas": (0.9, 0.999), 
             "lr": 3e-4,
-            "weight_decay": 5e-4,
+            "weight_decay": 5e-5,
         },
         "checkpoint_mode": "best",
         "loss_function": "SpotlightLossLogcosh",
