347de1f57640a1cc76245fd61ae781a2deb397a3
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-31 13:57:25 +0200

    hmm

--- models/cold_heart/configs/config_hyperparameters.py
+++ models/cold_heart/configs/config_hyperparameters.py
@@ -37,12 +37,12 @@ def get_hp_config():
         # --- Optimizer ---
         "optimizer_cls": "AdamW",
         "lr": 1e-4,
-        "weight_decay": 2e-4,
+        "weight_decay": 3e-4,
         "gradient_clip_val": 10.0,
         "optimizer_kwargs": {
             "betas": (0.9, 0.999), 
             "lr": 1e-4,
-            "weight_decay": 2e-4,
+            "weight_decay": 3e-4,
         },
 
         # --- LR Scheduler ---
--- models/revolving_door/configs/config_hyperparameters.py
+++ models/revolving_door/configs/config_hyperparameters.py
@@ -30,8 +30,8 @@ def get_hp_config():
         # Optimizer
         "optimizer_cls": "AdamW",
         "lr": 1e-4,
-        "weight_decay": 2e-4,
-        "gradient_clip_val": 50.0,
+        "weight_decay": 3e-4,
+        "gradient_clip_val": 10.0,
 
         # LR Scheduler
         "lr_scheduler_cls": "ReduceLROnPlateau",
@@ -53,7 +53,7 @@ def get_hp_config():
         "optimizer_kwargs": {
             "betas": (0.9, 0.999), 
             "lr": 1e-4,
-            "weight_decay": 2e-4,
+            "weight_decay": 3e-4,
         },
 
         # SpotlightLossLogcosh: logcosh base shape (gradient saturates at ±1)
