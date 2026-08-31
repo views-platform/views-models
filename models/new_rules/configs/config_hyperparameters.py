b8006f4deb908ebb252a1f8ff29adf1fd7f4dc80
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-31 16:38:47 +0200

    nr+ch

--- models/new_rules/configs/config_hyperparameters.py
+++ models/new_rules/configs/config_hyperparameters.py
@@ -9,14 +9,14 @@ def get_hp_config():
 
         # --- Architecture ---
         "generic_architecture": True,
-        "num_stacks": 2,
-        "num_blocks": 1,
+        "num_stacks": 1,
+        "num_blocks": 2,
         "num_layers": 2,
         "layer_widths": 64,
-        "expansion_coefficient_dim": 16,
+        "expansion_coefficient_dim": 64,
         "trend_polynomial_degree": 2,
         "activation": "ReLU",
-        "dropout": 0.4,
+        "dropout": 0.25,
         "batch_norm": False,
         "use_reversible_instance_norm": True,
         "use_static_covariates": True,
@@ -37,12 +37,12 @@ def get_hp_config():
         # --- Optimizer ---
         "optimizer_cls": "AdamW",
         "lr": 2e-4,
-        "weight_decay": 1e-3,
+        "weight_decay": 1e-4,
         "gradient_clip_val": 10.0,
         "optimizer_kwargs": {
             "betas": (0.9, 0.999), 
             "lr": 2e-4,
-            "weight_decay": 1e-3,
+            "weight_decay": 1e-4,
         },
 
         # --- LR Scheduler ---
